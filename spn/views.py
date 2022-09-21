6597c3f7dff59c356dc1ebac51cacee43b75ce66
BobBorges <robert.d.borges@gmail.com>
2022-09-21 15:02:21 +0200

    fixed file name

--- spn/views.py
+++ spn/views.py
@@ -51,62 +51,69 @@ def welcome_view(request, pic_set):
 
 @login_required
 def pic_view(request, pic_set, picnr):
-	UD_object = UserDetails.objects.get(user=request.user)
-	context = {"title": "MoReDaT Self-paced Picture Naming"}
-	current_set = SpnPictureSet.objects.get(slug=pic_set)
-	context['pic_set_obj'] = current_set
+    UD_object = UserDetails.objects.get(user=request.user)
+    context = {"title": "MoReDaT Self-paced Picture Naming"}
+    current_set = SpnPictureSet.objects.get(slug=pic_set)
+    context['pic_set_obj'] = current_set
+    
+    current_task = Tasks.objects.get(url_name='spn-welcome', url_arg=pic_set)
+    
+    pics = current_set.pictures.all().order_by('sort_order')	
+    pics_d = {}
+    for idx, pic in enumerate(pics, start=1):
+        pics_d[idx] = pic
 
-	current_task = Tasks.objects.get(url_name='spn-welcome', url_arg=pic_set)
+    current_pic = pics_d[int(picnr)]
+    context['current_pic'] = current_pic
 	
-	pics = current_set.pictures.all().order_by('sort_order')	
-	pics_d = {}
-	for idx, pic in enumerate(pics, start=1):
-		pics_d[idx] = pic
+    next_pic_nr = None
+    if int(picnr) < len(pics_d):
+        next_pic_nr = int(picnr) + 1
+    context['next_pic_nr'] = next_pic_nr
 
-	current_pic = pics_d[int(picnr)]
-	context['current_pic'] = current_pic
-	
-	next_pic_nr = None
-	if int(picnr) < len(pics_d):
-		next_pic_nr = int(picnr) + 1
-	context['next_pic_nr'] = next_pic_nr
+    if request.method == 'POST':
 
-	if request.method == 'POST':
+        form = SpnResponseForm(request.POST)
+        context['form'] = form
+        if form.is_valid():
+            if form.cleaned_data['response_type'] == 'Record response' and 'hidden_field' in request.FILES:
+                spn_filename = 'SPN-{}-{}_{}_{}_{:%Y-%m-%d-%H-%M-%S}.wav'.format(
+                    current_set.slug,
+                    picnr,
+                    UD_object.research_group.target_lang_short_name,
+                    request.user.id,
+                    datetime.now()
+                )
+                data = request.FILES['hidden_field']
+                path = default_storage.save(f'recorded-audio/{spn_filename}', ContentFile(data.read()))
+                tmp_file = os.path.join(settings.MEDIA_ROOT, path)
 
-		form = SpnResponseForm(request.POST)
-		context['form'] = form
-		if form.is_valid():
-			if form.cleaned_data['response_type'] == 'Record response' and 'hidden_field' in request.FILES:
-				spn_filename = f'SPN-{current_set.slug}-{picnr}_{UD_object.research_group.target_lang_short_name}_{request.user.id}_{datetime.now()}.wav'
-				data = request.FILES['hidden_field']
-				path = default_storage.save(f'recorded-audio/{spn_filename}', ContentFile(data.read()))
-				tmp_file = os.path.join(settings.MEDIA_ROOT, path)
+                f_inst = form.save(commit=False)
+                f_inst.task = current_task
+                f_inst.img = current_pic
+                f_inst.user = request.user
+                f_inst.audio_response = spn_filename
+                f_inst.save()
 
-				f_inst = form.save(commit=False)
-				f_inst.task = current_task
-				f_inst.img = current_pic
-				f_inst.user = request.user
-				f_inst.audio_response = spn_filename
-				f_inst.save()
-				if next_pic_nr:
-					return redirect('spn-pic', pic_set=pic_set, picnr=str(next_pic_nr))
-				else:
-					return redirect('spn-pic', pic_set=pic_set, picnr='finish')
-			else:
-				f_inst = form.save(commit=False)
-				f_inst.task = current_task
-				f_inst.img = current_pic
-				f_inst.user = request.user
-				f_inst.save()
-				if next_pic_nr:
-					return redirect('spn-pic', pic_set=pic_set, picnr=str(next_pic_nr))
-				else:
-					return redirect('spn-pic', pic_set=pic_set, picnr='finish')
+                if next_pic_nr:
+                    return redirect('spn-pic', pic_set=pic_set, picnr=str(next_pic_nr))
+                else:
+                    return redirect('spn-pic', pic_set=pic_set, picnr='finish')
+            else:
+                f_inst = form.save(commit=False)
+                f_inst.task = current_task
+                f_inst.img = current_pic
+                f_inst.user = request.user
+                f_inst.save()
+                if next_pic_nr:
+                    return redirect('spn-pic', pic_set=pic_set, picnr=str(next_pic_nr))
+                else:
+                    return redirect('spn-pic', pic_set=pic_set, picnr='finish')
 
-	else:
-		context['form'] = SpnResponseForm()
+    else:
+        context['form'] = SpnResponseForm()
 
-	return render(request, 'spn/spn-pic.html', context)
+    return render(request, 'spn/spn-pic.html', context)
 
 
 
